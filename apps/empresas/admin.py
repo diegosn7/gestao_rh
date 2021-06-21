09d1210762654668d85ba7da948327c2f20d4b92
diegonunes <eng.diego.nunes@gmail.com>
2021-06-20 23:35:03 -0300

    Criacao da app Funcionarios

--- apps/empresas/admin.py
+++ apps/empresas/admin.py
@@ -1,5 +1,6 @@
 # Register your models here.
 from django.contrib import admin
 from .models import Empresa
+from apps.funcionarios.models import Funcionario
 
 admin.site.register(Empresa)
---
+++ apps/funcionarios/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Funcionario(models.Model):
+    nome = models.CharField(max_length=100,help_text='Nome do Funcionário')
+
+    def __str__(self):
+        return self.nome
+
